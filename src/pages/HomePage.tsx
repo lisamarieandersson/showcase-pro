6d2e820c8adb0e3bcc2a067d92237a5f92ac241c
Lisa Marie Andersson <116926631+lisamarieandersson@users.noreply.github.com>
2024-05-26 13:55:11 +0200

    add boilerplate

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,15 @@
+// HomePage.tsx
+import { Link } from 'react-router-dom';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+      <Link to="/contact">Projects</Link>
+    </div>
+  );
+}
+
+export default HomePage;
