ef2267c239e3e45ba2daba72a108d3bc4c74678b
Lisa Marie Andersson <116926631+lisamarieandersson@users.noreply.github.com>
2024-05-26 14:14:56 +0200

    add routing set up

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createRoutesFromElements, Route } from 'react-router';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
+import ProjectPage from './pages/ProjectPage';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+/* ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+) */
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route index element={<HomePage />} />
+      <Route path="about" element={<AboutPage />} />
+      <Route path="contact" element={<ContactPage />} />
+      <Route path="projects" element={<ProjectPage />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
