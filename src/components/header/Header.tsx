e23ffe571d5ac2e6464a7fad43e7ec1031c0f42e
Lisa Marie Andersson <116926631+lisamarieandersson@users.noreply.github.com>
2024-05-27 23:35:07 +0200

    add and edit header

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+import Divider from '../divider/Divider';
+import './Header.scss';
+
+function Header() {
+  return (
+    <div className="header">
+      <Divider />
+    </div>
+  );
+}
+
+export default Header;
