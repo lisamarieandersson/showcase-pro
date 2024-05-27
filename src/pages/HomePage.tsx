2c9d51bf784bc833a903234004b6f49492af27f5
Lisa Marie Andersson <116926631+lisamarieandersson@users.noreply.github.com>
2024-05-27 02:02:34 +0200

    add nav menu

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,11 @@
 // HomePage.tsx
-import { Link } from 'react-router-dom';
+import NavigationMenu from '../components/navigation-menu/NavigationMenu';
 
 function HomePage() {
   return (
     <div>
       <h1>Home</h1>
-      <Link to="/about">About</Link>
-      <Link to="/cv">CV</Link>
-      <Link to="/contact">Contact</Link>
-      <Link to="/projects">Projects</Link>
+      <NavigationMenu />
     </div>
   );
 }
