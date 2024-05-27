e5f384aec4a993046ed78a4f53beed96c88587f7
Lisa Marie Andersson <116926631+lisamarieandersson@users.noreply.github.com>
2024-05-27 15:03:37 +0200

    add grid and components to homepage

--- src/pages/home-page/HomePage.tsx
+++ src/pages/home-page/HomePage.tsx
@@ -1,10 +1,13 @@
 // HomePage.tsx
+import Intro from '../../components/intro/Intro';
 import NavigationMenu from '../../components/navigation-menu/NavigationMenu';
+import './HomePage.scss';
 
 function HomePage() {
   return (
-    <div>
+    <div className="homepage">
       {/* <h1>Home</h1> */}
+      <Intro />
       <NavigationMenu />
     </div>
   );
