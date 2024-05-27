1a014423e3bbe25ff3593ef037c47df451e4eae5
Lisa Marie Andersson <116926631+lisamarieandersson@users.noreply.github.com>
2024-05-28 01:12:38 +0200

    add social icons component to footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,10 +1,12 @@
 import Divider from '../divider/Divider';
+import SocialIcons from '../social-icons/SocialIcons';
 import './Footer.scss';
 
 function Footer() {
   return (
     <div className="footer">
       <Divider />
+      <SocialIcons/>
     </div>
   );
 }
