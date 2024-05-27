c2b5438437ef5fb5c2a4f5d1d63fa89c2f1ef5ed
Lisa Marie Andersson <116926631+lisamarieandersson@users.noreply.github.com>
2024-05-27 15:02:36 +0200

    reorganize in folders

---
+++ src/components/intro/Intro.tsx
@@ -0,0 +1,12 @@
+import './Intro.scss';
+
+function Intro() {
+  return (
+    <div className="intro">
+      <h2 className="intro__name">Lisa Marie Andersson</h2>
+      <p className="intro__job-title">Junior Front End Developer</p>
+    </div>
+  );
+}
+
+export default Intro;
