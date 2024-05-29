5dc9ed012191b6d1587dc9f732d990f4697344f0
Lisa Marie Andersson <116926631+lisamarieandersson@users.noreply.github.com>
2024-05-29 23:47:31 +0200

    add basic code for contact page

--- src/pages/contact-page/ContactPage.tsx
+++ src/pages/contact-page/ContactPage.tsx
@@ -1,10 +1,14 @@
 // ContactPage.tsx
+import Footer from '../../components/footer/Footer';
+import Header from '../../components/header/Header';
 import './ContactPage.scss';
 
 function ContactPage() {
   return (
     <div className="contact-page">
-      <h1>Hello.</h1>
+      <Header />
+      <h1 className="contact-page__heading">Hello.</h1>
+      <Footer />
     </div>
   );
 }
