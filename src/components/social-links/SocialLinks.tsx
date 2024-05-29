9696130e79e93ad3b3a397ba8e46a0fe05a0b697
Lisa Marie Andersson <116926631+lisamarieandersson@users.noreply.github.com>
2024-05-30 01:38:34 +0200

    add social links component

--- src/components/social-links/SocialLinks.tsx
+++ src/components/social-links/SocialLinks.tsx
@@ -1,9 +1,37 @@
 import './SocialLinks.scss';
 
+
+const links = [
+  {
+    name: 'Github',
+    url: 'https://github.com/lisamarieandersson',
+  },
+  {
+    name: 'Linkedin',
+    url: 'https://www.linkedin.com/in/lisa-marie-andersson-a11682140/',
+  },
+  {
+    name: 'Instagram',
+    url: 'https://www.instagram.com/upthewoodenhills/',
+  },
+];
+
 function SocialLinks() {
-    return(
-        <div></div>
-    );
+  return (
+    <div className="social-links">
+      {links.map((link) => (
+        <a
+          key={link.name}
+          className="social-links__link"
+          href={link.url}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {link.name} <img className="social-links__icon" src="/icons/arrow-icon.svg" alt="" />
+        </a>
+      ))}
+    </div>
+  );
 }
 
 export default SocialLinks;
