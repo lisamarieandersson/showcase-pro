f1e662a51e393ed5f3b2e64a857a065aae5dc921
Lisa Marie Andersson <116926631+lisamarieandersson@users.noreply.github.com>
2024-06-09 22:59:26 +0200

    add iframe for bigger screens with cv as pdf

--- src/pages/cv-page/CvPage.tsx
+++ src/pages/cv-page/CvPage.tsx
@@ -1,9 +1,17 @@
 //CvPage.tsx
+import './CvPage.scss';
 
 function CvPage() {
   return (
-    <div>
-      <h1>CV</h1>
+    <div className="cv-page">
+      <iframe
+        className="cv-page__iframe"
+        src="/assets/lisamarie-andersson-cv-2024.pdf"
+        title="My CV"
+      >
+        Your browser does not support PDFs. Please download the PDF to view it:
+        <a href="/assets/lisamarie-andersson-cv-2024.pdf">Download PDF</a>
+      </iframe>
     </div>
   );
 }
