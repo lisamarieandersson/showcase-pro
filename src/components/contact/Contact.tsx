53e6f08a14115a23cf8b39c1175b0a6e4086c8f0
Lisa Marie Andersson <116926631+lisamarieandersson@users.noreply.github.com>
2024-06-12 00:14:48 +0200

    edit contact component

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -5,6 +5,10 @@ function Contact() {
     <div className="contact">
       <h1 className="contact__heading">Hello.</h1>
       <p className="contact__text">Get in touch.</p>
+      <p className="contact__email-text">Email:</p>
+      <a href="mailto:lisamarie.andersson@gmail.com" className="contact__email">
+        lisamarie.andersson@gmail.com
+      </a>
     </div>
   );
 }
