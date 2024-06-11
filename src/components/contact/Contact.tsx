551fca73ff066f1cf3bbf8435f26e6d5d2bb73e8
Lisa Marie Andersson <116926631+lisamarieandersson@users.noreply.github.com>
2024-06-11 23:54:40 +0200

    add contact component file

---
+++ src/components/contact/Contact.tsx
@@ -0,0 +1,12 @@
+import './Contact.scss';
+
+function Contact() {
+  return (
+    <div className="contact">
+      <h1 className="contact__heading">Hello.</h1>
+      <p className="contact__text">Get in touch.</p>
+    </div>
+  );
+}
+
+export default Contact;
