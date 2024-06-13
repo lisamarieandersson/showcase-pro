ffce43b66ac916935a5aa573f0b1b0789f98486c
Lisa Marie Andersson <116926631+lisamarieandersson@users.noreply.github.com>
2024-06-13 23:46:32 +0200

    add contact email etc

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -5,10 +5,38 @@ function Contact() {
     <div className="contact">
       <h1 className="contact__heading">Hello.</h1>
       <p className="contact__text">Get in touch.</p>
-      <p className="contact__email-text">Email:</p>
-      <a href="mailto:lisamarie.andersson@gmail.com" className="contact__email">
-        lisamarie.andersson@gmail.com
-      </a>
+      <div className="contact__email">
+        <p className="contact__email-text">Email:&nbsp;</p>
+        <a
+          href="mailto:lisamarie.andersson@gmail.com"
+          className="contact__email-address"
+        >
+          lisamarie.andersson@gmail.com
+        </a>
+      </div>
+      <div className="contact__internet">
+        <p className="contact__internet-text">On the Internet:</p>
+        <a
+          href="https://www.linkedin.com/in/lisa-marie-andersson-a11682140/"
+          className="contact__internet-link"
+        >
+          Linkedin
+        </a>
+        <p className="contact__internet-text">/</p>
+        <a
+          href="https://github.com/lisamarieandersson"
+          className="contact__internet-link"
+        >
+          Github
+        </a>
+        <p className="contact__internet-text">/</p>
+        <a
+          href="https://www.instagram.com/upthewoodenhills/"
+          className="contact__internet-link"
+        >
+          Instagram
+        </a>
+      </div>
     </div>
   );
 }
