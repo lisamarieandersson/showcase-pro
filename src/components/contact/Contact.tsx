b5410fc4eede7eacd43a59428b440acbc51a0085
Lisa Marie Andersson <116926631+lisamarieandersson@users.noreply.github.com>
2024-06-15 01:38:36 +0200

    edit css and refactor component

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,3 +1,4 @@
+import SocialLinks from '../social-links/SocialLinks';
 import './Contact.scss';
 
 function Contact() {
@@ -6,7 +7,6 @@ function Contact() {
       <h1 className="contact__heading">Hello.</h1>
       <p className="contact__text">Get in touch.</p>
       <div className="contact__email">
-        <p className="contact__email-text">Email:&nbsp;</p>
         <a
           href="mailto:lisamarie.andersson@gmail.com"
           className="contact__email-address"
@@ -14,29 +14,7 @@ function Contact() {
           lisamarie.andersson@gmail.com
         </a>
       </div>
-      <div className="contact__internet">
-        <p className="contact__internet-text">On the Internet:</p>
-        <a
-          href="https://www.linkedin.com/in/lisa-marie-andersson-a11682140/"
-          className="contact__internet-link"
-        >
-          Linkedin
-        </a>
-        <p className="contact__internet-text">/</p>
-        <a
-          href="https://github.com/lisamarieandersson"
-          className="contact__internet-link"
-        >
-          Github
-        </a>
-        <p className="contact__internet-text">/</p>
-        <a
-          href="https://www.instagram.com/upthewoodenhills/"
-          className="contact__internet-link"
-        >
-          Instagram
-        </a>
-      </div>
+      <SocialLinks />
     </div>
   );
 }
