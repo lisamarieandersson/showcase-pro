815fc51757d5716ec364bbc96baf05f0a329c38a
Lisa Marie Andersson <116926631+lisamarieandersson@users.noreply.github.com>
2024-06-21 23:25:27 +0200

    add useRef for animation effect of drop down

--- src/components/dropdown-menu/DropDownMenu.tsx
+++ src/components/dropdown-menu/DropDownMenu.tsx
@@ -1,23 +1,32 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './DropDownMenu.scss';
 import hamburgerIcon from '/icons/hamburger-menu-icon.svg';
 
 function DropDownMenu() {
   const [isOpen, setIsOpen] = useState(false);
+  const contentRef = useRef<HTMLDivElement>(null);
   const toggleOpen = () => setIsOpen(!isOpen);
+
+  useEffect(() => {
+    if (isOpen && contentRef.current) {
+      contentRef.current.style.maxHeight = `${contentRef.current.scrollHeight}px`;
+    } else if (contentRef.current) {
+      contentRef.current.style.maxHeight = '0';
+    }
+  }, [isOpen]);
 
   return (
     <div className="dropdown-menu">
       <button className="dropdown-menu__button" onClick={toggleOpen}>
         <img className="dropdown-menu__icon" src={hamburgerIcon} alt="Menu" />
       </button>
-      {isOpen && (
-        <nav
-          className={`dropdown-menu__content ${
-            isOpen ? 'dropdown-menu__content--open' : ''
-          }`}
-        >
+      <nav
+        ref={contentRef}
+        className={`dropdown-menu__content ${
+          isOpen ? 'dropdown-menu__content--open' : ''
+        }`}
+      >
           <ul className="dropdown-menu__list">
             <li className="dropdown-menu__item">
               <Link className="dropdown-menu__link" to="/">
@@ -46,7 +55,6 @@ function DropDownMenu() {
             </li>
           </ul>
         </nav>
-      )}
     </div>
   );
 }
