c831286a3fc2036cfdd0eafe25db9c77951cab63
Lisa Marie Andersson <116926631+lisamarieandersson@users.noreply.github.com>
2024-07-01 16:24:46 +0200

    move iframe to cv page swedish and english

--- src/pages/cv-page/CvPage.tsx
+++ src/pages/cv-page/CvPage.tsx
@@ -1,17 +1,14 @@
 //CvPage.tsx
 import './CvPage.scss';
+import Header from '../../components/header/Header';
+import Footer from '../../components/footer/Footer';
 
 function CvPage() {
   return (
     <div className="cv-page">
-      <iframe
-        className="cv-page__iframe"
-        src="/assets/lisamarie-andersson-cv-2024.pdf"
-        title="My CV"
-      >
-        Your browser does not support PDFs. Please download the PDF to view it:
-        <a href="/assets/lisamarie-andersson-cv-2024.pdf">Download PDF</a>
-      </iframe>
+      <Header />
+        <h1 className="cv-page__heading">CV</h1>
+      <Footer/>
     </div>
   );
 }
