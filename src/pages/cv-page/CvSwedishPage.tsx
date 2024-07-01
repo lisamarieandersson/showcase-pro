99e727d168297065b491e66a1a29d34596c6ca42
Lisa Marie Andersson <116926631+lisamarieandersson@users.noreply.github.com>
2024-07-01 16:25:22 +0200

    add iframe for cv page in swedish

---
+++ src/pages/cv-page/CvSwedishPage.tsx
@@ -0,0 +1,18 @@
+import './CvSwedishPage.scss';
+
+function CvSwedishPage() {
+    return (
+        <div className="cv-swedish-page">
+          <iframe
+            className="cv-swedish-page__iframe"
+            src="/assets/lisamarie-andersson-cv-2024.pdf"
+            title="My CV"
+          >
+            Your browser does not support PDFs. Please download the PDF to view it:
+            <a href="/assets/lisamarie-andersson-cv-2024.pdf">Download PDF</a>
+          </iframe>
+        </div>
+      );
+}
+
+export default CvSwedishPage;
