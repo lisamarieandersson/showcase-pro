f61783c9003c2a9e7b1a87ca4cd657729b14e520
Lisa Marie Andersson <116926631+lisamarieandersson@users.noreply.github.com>
2024-07-03 01:52:04 +0200

    change to single quote

--- src/pages/cv-page/CvEnglishPage.tsx
+++ src/pages/cv-page/CvEnglishPage.tsx
@@ -1,18 +1,18 @@
 import './CvEnglishPage.scss';
 
 function CvEnglishPage() {
-    return(
-        <div className="cv-english-page">
-          <iframe
-            className="cv-english-page__iframe"
-            src="/assets/lisamarie-andersson-cv-2024.pdf"
-            title="My CV"
-          >
-            Your browser does not support PDFs. Please download the PDF to view it:
-            <a href="/assets/lisamarie-andersson-cv-2024.pdf">Download PDF</a>
-          </iframe>
-        </div>
-    );
+  return (
+    <div className='cv-english-page'>
+      <iframe
+        className='cv-english-page__iframe'
+        src='/assets/lisamarie-andersson-cv-2024.pdf'
+        title='My CV'
+      >
+        Your browser does not support PDFs. Please download the PDF to view it:
+        <a href='/assets/lisamarie-andersson-cv-2024.pdf'>Download PDF</a>
+      </iframe>
+    </div>
+  );
 }
 
 export default CvEnglishPage;
