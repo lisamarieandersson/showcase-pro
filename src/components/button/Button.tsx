b0cdfcd1341b2b3cd362ceefda82a7f69982827a
Lisa Marie Andersson <116926631+lisamarieandersson@users.noreply.github.com>
2024-07-02 23:50:24 +0200

    add button component

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,13 @@
+import './Button.scss'; 
+
+interface ButtonProps {
+    label: string;
+}
+
+function Button({label}: ButtonProps) {
+    return(
+        <button className='button'>{label}</button>
+    );
+}
+
+export default Button;
--- src/pages/cv-page/CvPage.tsx
+++ src/pages/cv-page/CvPage.tsx
@@ -3,12 +3,15 @@ import './CvPage.scss';
 import Header from '../../components/header/Header';
 import Footer from '../../components/footer/Footer';
 
+
 function CvPage() {
   return (
     <div className="cv-page">
       <Header />
-        <h1 className="cv-page__heading">CV</h1>
-      <Footer/>
+      <h1 className="cv-page__heading">CV</h1>
+      {/*       <Button label="Swedish" />
+      <Button label="English" /> */}
+      <Footer />
     </div>
   );
 }
