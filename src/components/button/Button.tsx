5544a99af800d2202ce21789f3d90072f534414e
Lisa Marie Andersson <116926631+lisamarieandersson@users.noreply.github.com>
2024-07-04 12:11:55 +0200

    add react router link to button component

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,11 +1,13 @@
+import { Link } from 'react-router-dom';
 import './Button.scss';
 
 interface ButtonProps {
   label: string;
+  to: string;
 }
 
-function Button({ label }: ButtonProps) {
-  return <button className="button">{label}</button>;
+function Button({ label, to }: ButtonProps) {
+  return <Link to={to} className="button">{label}</Link>;
 }
 
 export default Button;
