dad0a2e7f7be0c4a7f9062638cc0e8701008cd65
Lisa Marie Andersson <116926631+lisamarieandersson@users.noreply.github.com>
2024-07-30 23:52:29 +0200

    add link to cv in english

--- src/pages/cv-page/CvEnglishPage.tsx
+++ src/pages/cv-page/CvEnglishPage.tsx
@@ -1,16 +1,15 @@
 import './CvEnglishPage.scss';
 
-//TODO: Add English version of CV
 function CvEnglishPage() {
   return (
     <div className="cv-english-page">
       <iframe
         className="cv-english-page__iframe"
-        src="/assets/lisamarie-andersson-cv-2024.pdf"
+        src="/assets/lisamarie-andersson-cv-english.pdf"
         title="My CV"
       >
         Your browser does not support PDFs. Please download the PDF to view it:
-        <a href="/assets/lisamarie-andersson-cv-2024.pdf">Download PDF</a>
+        <a href="/assets/lisamarie-andersson-cv-english.pdf">Download PDF</a>
       </iframe>
     </div>
   );
