c58cccf7e2728563deaed2c8831960aef1fbb323
Lisa Marie Andersson <116926631+lisamarieandersson@users.noreply.github.com>
2024-08-05 01:12:33 +0200

    add media queries from pdf to png

--- src/pages/cv-page/CvEnglishPage.tsx
+++ src/pages/cv-page/CvEnglishPage.tsx
@@ -11,6 +11,10 @@ function CvEnglishPage() {
         Your browser does not support PDFs. Please download the PDF to view it:
         <a href="/assets/lisamarie-andersson-cv-english.pdf">Download PDF</a>
       </iframe>
+      <div className="cv-english-page__images">
+        <img src="/assets/lisamarie-andersson-cv-english-1.png" alt="CV Page 1" />
+        <img src="/assets/lisamarie-andersson-cv-english-2.png" alt="CV Page 2" />
+      </div>
     </div>
   );
 }
