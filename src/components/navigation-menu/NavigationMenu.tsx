5fb0f0db0e3be13744c74509a33fc74c2ed250ea
Lisa Marie Andersson <116926631+lisamarieandersson@users.noreply.github.com>
2024-08-06 23:49:53 +0200

    remove home link, not needed on home page

--- src/components/navigation-menu/NavigationMenu.tsx
+++ src/components/navigation-menu/NavigationMenu.tsx
@@ -2,7 +2,6 @@ import { Link } from 'react-router-dom';
 import './NavigationMenu.scss';
 
 const links = [
-  { path: '/', label: 'Home' },
   { path: '/about', label: 'About' },
   { path: '/cv', label: 'CV' },
   { path: '/contact', label: 'Contact' },
@@ -13,13 +12,13 @@ function NavigationMenu() {
   return (
     <nav className="navigation-menu">
       <ul className="navigation-menu__list">
-      {links.map((link, index) => (
-            <li key={index} className="dropdown-menu__item">
-              <Link className="dropdown-menu__link" to={link.path}>
-                {link.label}
-              </Link>
-            </li>
-          ))}
+        {links.map((link, index) => (
+          <li key={index} className="dropdown-menu__item">
+            <Link className="dropdown-menu__link" to={link.path}>
+              {link.label}
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
