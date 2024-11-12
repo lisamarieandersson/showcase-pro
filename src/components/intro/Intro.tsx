21a0803a78d01785e3027e1be8672dc40f48c6a6
Lisa Marie Andersson <116926631+lisamarieandersson@users.noreply.github.com>
2024-11-13 00:13:25 +0100

    style: edit presentation

--- src/components/intro/Intro.tsx
+++ src/components/intro/Intro.tsx
@@ -4,7 +4,7 @@ function Intro() {
   return (
     <div className="intro">
       <h1 className="intro__name">Lisa Marie Andersson</h1>
-      <p className="intro__job-title">Front-End Developer</p>
+      <p className="intro__job-title">Frontend Developer</p>
     </div>
   );
 }
